c0c95ff30d76b18e0423993f9b80dceab1e8d24a
Lucca Trevisan <141439341+luccatrevisan@users.noreply.github.com>
2025-07-18 10:01:28 -0300

    atualizando limite numeroSecreto

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ GIT COMMANDS
 */
 
 let listaDeNumerosSorteados = []; //tem que estar antes da chamada de gerarNumeroAleatorio() porque o JS executa em ordem
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -30,7 +30,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
